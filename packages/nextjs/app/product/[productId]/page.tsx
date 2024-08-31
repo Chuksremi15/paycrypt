eaad057ca999f4a2b67242377af2cb15c0a8a2d1
chuksremi15 <chuksremi15@gmail.com>
2024-09-01 00:18:58 +0100

    product page cleaned up and completed

--- packages/nextjs/app/product/[productId]/page.tsx
+++ packages/nextjs/app/product/[productId]/page.tsx
@@ -1,42 +1,87 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import { parseEther } from "viem";
+import { TextInput, TextSelect } from "~~/components/pop-up-store/molecules/Form";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const page = () => {
   const { resolvedTheme } = useTheme();
   const isDarkMode = resolvedTheme === "dark";
+  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("PopUpStore");
+
+  const [addTokenLoading, setPaymentLoading] = useState<boolean>(false);
+
+  const { data: tokens } = useScaffoldReadContract({
+    contractName: "PopUpStore",
+    functionName: "getPaymentTokens",
+  });
+
+  const [paymentForm, setPaymentForm] = useState<{ tokenName: string }>({
+    tokenName: "",
+  });
+
+  const onTokenChange = (value: string, formKey: string) => {
+    setPaymentForm(form => ({ ...form, [formKey]: value }));
+  };
+
+  const handlePayment = async () => {
+    try {
+      console.log("form values: ", paymentForm);
+
+      if (paymentForm.tokenName) {
+        setPaymentLoading(true);
+        const trxRef = await writeYourContractAsync({
+          functionName: "payForProduct",
+          args: [parseEther("50"), paymentForm.tokenName, "3627873bfdnfdnfnoifoiieefifeoioe"],
+        });
+
+        if (trxRef) {
+          console.log("payment successful");
+        }
+
+        setPaymentLoading(false);
+      }
+    } catch (e) {
+      console.error("Error setting:", e);
+      setPaymentLoading(false);
+    }
+  };
 
   return (
     <div className="container mx-auto max-w-4xl py-8 px-8">
-      <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:mt-[0px]">
-        <div className="row-start-2 lg:row-start-1">
-          <h4 className="font-heading text-xl leading-[30px] lg:max-w-[750px] lg:text-[55px] lg:leading-[75px]">
-            GRAPHIC T-SHIRT
-          </h4>
-          <div className="mt-3 text-xl tracking-wide lg:mt-5 lg:text-2xl">
-            <span className="text-[12px] font-normal lg:text-[20px]">By</span> Chukwuma Didi
-          </div>
-          <i className="text-[12px] font-normal lg:text-[17px]">Acrylic canvas</i>
-          <div className="my-2 text-[12px]  lg:my-3 lg:text-[17px]">100 X 180cm 44 X 70 in</div>
-          <div>
-            <div className="mt-[24px] max-w-[400px] text-[14px] lg:mt-[34px] lg:text-[20px] lg:leading-[25px]">
-              <p className="text-[15px] lg:text-[18px]">About this artwork</p>
-              <p className="pt-4">
-                <b className="font-heading">"Ashes Dance" </b>
-                In this captivating masterpiece, we are transported to an epic universe of romance, where love and
-                nature intertwine in a harmonious dance. Each brushstroke and color palette are a celebration of these
-                timeless forces, weaving a narrative that transcends time and space.
-              </p>
-            </div>
-            <div className="mt-4 mb-10 lg:my-3 lg:mt-0 lg:mb-0">
-              <p className="font-heading text-[18px] lg:text-[30px]">$50</p>
-            </div>
-            <Button text="Pay Now" isDarkMode={isDarkMode} />
-          </div>
+      <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 ">
+        <div className="relative w-full h-[300px] lg:h-[400px]">
+          <img className="w-full h-full object-cover" alt="art work" width={400} height={400} src="/store/shirt.jpg" />
         </div>
-        <div className="relative w-full h-[300px] lg:h-[650px]">
-          <img className="w-full h-full " alt="art work" width={400} height={400} src="/store/artwork.png" />
+        <div className="">
+          <h4 className="font-heading text-xl opacity-80">
+            Vamtac Graphic Tees Mens Vintage Oversized T Shirts Half Sleeve Boxy Baggy Loose Streetwear
+          </h4>
+          <p className="font-body text-2xl">$59.99</p>
+          <p className="text-body text-lg">About this item</p>
+          <p className="text-body">
+            Soft and comfortable: Made of 100% pure cotton, this t shirt is soft, lightweight and comfortable to wear.
+            The natural material is breathable, helping to regulate body temperature and preventing sweat buildup.
+            Graphic T Shirts: The fashion element of this oversized t shirt is butterfly print, it is funny and cute,
+            popular among young people, also a great gift. Oversized Shirts: The shirt's oversized fit is loose and
+            relaxed, providing maximum comfort and allowing for easy
+          </p>
+
+          <div className="flex flex-col gap-y-2">
+            <TextSelect
+              type="text"
+              placeholder="Token name"
+              name="tokenName"
+              isDarkMode={isDarkMode}
+              value={paymentForm.tokenName}
+              onChange={onTokenChange}
+              tokens={tokens}
+            />
+            <Button text="Pay Now" isDarkMode={isDarkMode} loading={addTokenLoading} action={() => handlePayment()} />
+          </div>
         </div>
       </div>
     </div>
--- packages/nextjs/components/pop-up-store/molecules/Form/index.tsx
+++ packages/nextjs/components/pop-up-store/molecules/Form/index.tsx
@@ -39,3 +39,56 @@ export const TextInput = ({
     </div>
   );
 };
+
+export const TextSelect = ({
+  type,
+  name,
+  value,
+  placeholder,
+  onChange,
+  isDarkMode,
+  tokens,
+}: {
+  type: React.HTMLInputTypeAttribute;
+  name: string;
+  value: string | number;
+  placeholder: string;
+  onChange: (newValue: string, newName: string) => void;
+  isDarkMode: boolean;
+  tokens:
+    | readonly {
+        tokenName: string;
+        tokenAddress: string;
+      }[]
+    | undefined;
+}) => {
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      onChange(e.target.value, e.target.name);
+    },
+    [onChange],
+  );
+
+  const options = tokens?.slice();
+
+  options?.push({ tokenName: "Ethereum", tokenAddress: "" });
+
+  return (
+    <div>
+      <select
+        //type={type}
+        name={name}
+        //placeholder={placeholder}
+        onChange={handleChange}
+        value={value}
+        className={`w-full ${
+          isDarkMode ? "border border-white " : "border border-black"
+        } rounded-none py-3 px-4 bg-base-200  text-md focus:outline-none  transition-all duration-500`}
+        autoComplete="off"
+      >
+        <option value="">Select token to pay with</option>
+        {options && options.map(({ tokenName }) => <option value={tokenName}>{tokenName}</option>)}
+      </select>
+    </div>
+  );
+};
